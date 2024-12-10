344da616147e93db1117b961e1d62895f61dd1b3
Afif Rizki Putra <afifputra21.ap@gmail.com>
2024-12-10 09:13:51 +0700

    delete promo button

---
+++ script.js
@@ -0,0 +1,20 @@
+let slideIndex = 0;
+
+function moveSlide(direction) {
+    const carousel = document.querySelector('.carousel');
+    const totalSlides = document.querySelectorAll('.carousel img').length;
+    const slideWidth = document.querySelector('.carousel img').clientWidth;
+
+    slideIndex += direction;
+
+    if (slideIndex >= totalSlides) {
+        slideIndex = 0;
+    } else if (slideIndex < 0) {
+        slideIndex = totalSlides - 1;
+    }
+
+    console.log(totalSlides);
+    console.log(slideIndex);
+
+    carousel.style.transform = `translateX(${-slideIndex * slideWidth *0.67}px)`;
+}
